470007bc5c29eb2df576980e405ecbe413f2c7c8
kauesanderson <kauesanderson3112@gmail.com>
2026-09-01 22:52:34 -0300

    Finalizando condições em JS

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var nota = 5
+console.log (`Sua nota foi ${nota}.`)
+if ( nota >= 7) {
+    console.log ('Aprovado')
+} else if (nota >= 5) {
+    console.log ('Recuperação')
+    }
+    else {
+        console.log ('Reprovado')
+    }
